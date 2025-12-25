ebd842b8b38d00a16157a0d4445c4daf44dd38d2
ypjcode <1548188756@qq.com>
2025-12-25 23:49:08 +0800

    在src文件夹下添加telegram文件夹，实现裂变，买币，查倒计时，获得空投代币，并在footer里加上链接

--- tws/src/main.jsx
+++ tws/src/main.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import App from './App';
 import './index.css';
+
+// 2. 配置 TON Connect 清单地址（测试用，上线替换为自己的）
+// 测试清单地址（官方示例），上线需替换为你自己的 manifest.json 地址
+const tonManifestUrl = 'https://ton-connect.github.io/demo-dapp/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+    {/* 添加 TonConnectUIProvider 包裹 AuthProvider + App */}
+      <TonConnectUIProvider manifestUrl={tonManifestUrl}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </TonConnectUIProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
